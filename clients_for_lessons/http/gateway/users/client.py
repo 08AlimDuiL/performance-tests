cb62fa5b20b2d56adaed31fe33f0bbcc61e85b50
Liudmila Portnova <portnovaliudmila@gmail.com>
2025-09-19 10:30:03 +0300

    Практикуемся в использовании HTTP API клиентов (#7)
    * Практикуемся в реализации HTTP API клиентов
    
    * Практикуемся в использовании HTTP API клиентов

--- clients_for_lessons/http/gateway/users/client.py
+++ clients_for_lessons/http/gateway/users/client.py
@@ -1,6 +1,29 @@
+import time
 from httpx import Response
 from clients_for_lessons.http.client import HTTPClient
 from typing import TypedDict
+from clients_for_lessons.http.gateway.client import build_gateway_http_client  # Импортируем builder
+
+
+# Добавили описание структуры пользователя
+class UserDict(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+    phoneNumber: str
+
+
+# Добавили описание структуры ответа получения пользователя
+class GetUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа получения пользователя.
+    """
+    user: UserDict
 
 
 class CreateUserRequestDict(TypedDict):
@@ -12,6 +35,14 @@ class CreateUserRequestDict(TypedDict):
     firstName: str
     middleName: str
     phoneNumber: str
+
+
+# Добавили описание структуры ответа создания пользователя
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: UserDict
 
 
 class UsersGatewayHTTPClient(HTTPClient):
@@ -36,3 +67,31 @@ class UsersGatewayHTTPClient(HTTPClient):
         :return: Ответ от сервера (объект httpx.Response).
         """
         return self.post("/api/v1/users", json=request)
+
+    # Добавили новый метод
+    def get_user(self, user_id: str) -> GetUserResponseDict:
+        response = self.get_user_api(user_id)
+        return response.json()
+
+    # Добавили новый метод
+    def create_user(self) -> CreateUserResponseDict:
+        request = CreateUserRequestDict(
+            email=f"user.{time.time()}@example.com",
+            lastName="string",
+            firstName="string",
+            middleName="string",
+            phoneNumber="string"
+        )
+        response = self.create_user_api(request)
+        return response.json()
+
+    # Добавляем builder для UsersGatewayHTTPClient
+
+
+def build_users_gateway_http_client() -> UsersGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр UsersGatewayHTTPClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию UsersGatewayHTTPClient.
+    """
+    return UsersGatewayHTTPClient(client=build_gateway_http_client())
